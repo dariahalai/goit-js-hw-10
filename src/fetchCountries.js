aa04d4aad5120a7fad32ad69143d28ae634a821a
Halai Daria <halaidaria@gmail.com>
2022-11-13 00:19:46 +0200

    my-work

---
+++ src/fetchCountries.js
@@ -0,0 +1,61 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+    countryInfo : document.querySelector('.country-info'),
+    countryList: document.querySelector('.country-list'),
+    input : document.getElementById('search-box')
+}
+
+export function fetchCountries(name){
+    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+   .then(response => {
+    if(!response.ok){
+    throw new Error(response.status)
+    }
+    return response.json()})
+   .then(data => {
+    const oneMatch = data.length === 1;
+    const maxMatch = 10;
+       // console.log(data)
+        if(oneMatch){
+           const markup = data.map(country => 
+               `<li class="country-item">
+               <div class="country-item__main">
+               <img class="country-item__flag"src="${country.flags.svg}" alt="flag ${country.name.official}">
+               <h1 class="country-item__name">${country.name.official}</h1>
+               </div>
+                <div class="country-item__info">
+               <p class="country-item__capital">Capital :${country.capital}</p>
+               <p class="country-item__population">Population :${country.population}</p>
+               <p class="country-item__languages">Languages :${country.languages}</p>
+               </li>
+               `).join('')
+               refs.countryList.innerHTML = markup;
+               return;
+           }
+           else if(data.length <= maxMatch) {
+               const markup = data.map(country => 
+                   `<li class="country-item">
+                   <div class="country-item__main">
+                   <img class="country-item__flag"src="${country.flags.svg}" alt="flag ${country.name.official}">
+                   <h1 class="country-item__name">${country.name.official}</h1>
+                   </div>
+               `).join('')
+               refs.countryList.innerHTML = markup;
+               return;
+           }
+           else if(data.length > maxMatch){
+            refs.countryList.innerHTML = '';
+               Notify.info("Too many matches found. Please enter a more specific name.");
+           }
+               
+  
+   })
+   .catch(error => {
+    if (error.message === '404') {
+        refs.countryList.innerHTML = '';
+        Notify.failure('Oops, there is no country with that name');
+      }
+      console.log(error);
+   })
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './css/styles.css';
+import debounce from "lodash.debounce";
+import {fetchCountries} from './fetchCountries.js'
+
+const refs = {
+    countryInfo : document.querySelector('.country-info'),
+    countryList: document.querySelector('.country-list'),
+    input : document.getElementById('search-box')
+}
+
 
 const DEBOUNCE_DELAY = 300;
+
+refs.input.addEventListener('input',debounce(onSearch,DEBOUNCE_DELAY))
+
+function onSearch(e) {
+let name = e.target.value;
+let NAME = name.trim().toLowerCase();
+fetchCountries(NAME)
+if(NAME === ''){
+    refs.countryList.innerHTML = '';
+}
+}
+
